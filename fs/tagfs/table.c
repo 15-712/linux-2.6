e8007ab75c60e942caaa5415b4f6c1d795e23a45
Tim Shields <tshield@gmail.com>
2011-03-07 20:54:23 -0500

    Hash table implementation of tag lookup structure

--- fs/tagfs/table.c
+++ fs/tagfs/table.c
@@ -1,93 +1,157 @@
-/* 
-* Tag lookup table implementation
+/** @file table.c
+*   @brief Hash table for the tags. 
+*   
+*   Tag lookup table implementation. Each bucket contains a list of inodes
+*   stored in a table_element data structure.
+*
 */
 
-//int remove(char* tag, inode_entry *e);
-
 #include "table_element.h"
+#include <linux/hash.h>
+#include <linux/slab.h>
 
 #define MAX_TAG_LEN	255
+#define NUM_HASH_BITS	12
 
-struct tag_tree_node {
+struct tag_node {
 	char tag[MAX_TAG_LEN];
 	struct table_element *e;
-	struct tag_tree left;
-	struct tag_tree right;
 	int tag_id;
-}	
-
-enum tag_tree_error {
-	INVALID_NODE = -255,
-	ALREADY_EXISTS,	
+	struct tag_node *next;
 };
 
-struct tag_tree_node *find_node(struct tag_tree_node *node, char *tag) 
+struct tag_name_id {
+	int id;
+	char tag[MAX_TAG_LEN];
+	struct tag_name_id *next;
+};
+
+enum tag_node_error {
+	INVALID_NODE = -255,
+};
+
+/* 
+*  String hashing function taken from linux/sunrpc/avcauth.h
+*/
+static inline unsigned long hash_tag(const char *name)
 {
-	if(!node)
+	unsigned long hash = 0;
+	unsigned long l = 0;
+	int len = 0;
+	unsigned char c;
+	do {
+		if (unlikely(!(c = *name++))) {
+			c = (char)len; len = -1;
+		}
+		l = (l << 8) | c;
+		len++;
+		if ((len & (BITS_PER_LONG/8-1))==0)
+			hash = hash_long(hash^l, BITS_PER_LONG);
+	} while (len);
+	return hash >> (BITS_PER_LONG - NUM_HASH_BITS);
+}
+
+struct tag_name_id *create_new_lookup(struct tag_name_id *head, const char *tag)
+{
+	struct tag_name_id *t; 
+	t = NULL;
+	if(head == NULL) {
+		t = kmalloc(sizeof(struct tag_name_id), GFP_KERNEL);
+		strlcpy(t->tag, tag, MAX_TAG_LEN);
+		t->id = 1;
+	} else if(head->next == NULL) {
+		t = kmalloc(sizeof(struct tag_name_id), GFP_KERNEL);
+		strlcpy(t->tag, tag, MAX_TAG_LEN);
+		t->id = head->id+1;
+		head->next = t;
+	} else if(head->next->id > head->id+1) {
+		t = kmalloc(sizeof(struct tag_name_id), GFP_KERNEL);
+		strlcpy(t->tag, tag, MAX_TAG_LEN);
+		t->id = head->id+1;
+		t->next = head->next;
+		head->next = t;
+	}
+	return t;
+}
+
+struct tag_node *find_node(struct tag_node **head, const char *tag) 
+{
+	struct tag_node *node;
+	if(!head)
 		return NULL;
-	int compare = strncmp(node->tag, tag, MAX_TAG_LEN);
-	if(compare == 0)
-		return node;	
-	if(compare < 0)
-		return find_node(node->left, tag);	
-	if(compare > 0)
-		return find_node(node->right, tag);	
+	node = head[hash_tag(tag)];
+	while(node != NULL && strncmp(node->tag, tag, MAX_TAG_LEN) != 0) {
+		node = node->next;
+	}
+	return node;
 }
 
-int result add_node(struct tag_tree_node *root, struct tag_tree_node *new_node) 
+int add_node(struct tag_node **head, struct tag_node *new_node) 
 {
-	if(!root)
+	struct tag_node *node;
+	if(!head)
 		return INVALID_NODE;
-	int compare = strncmp(root->tag, new_node->tag, MAX_TAG_LEN);
-	if(compare == 0)
-		return ALREADY_EXISTS;
-
-	/* Add new tag node to the left */
-	if(compare < 0) {
-		if(root->left == NULL) {
-			root->left = new_node;
-			return 0;
-		} else {
-			return add_node(root->left, tag);	
+	node = head[hash_tag(new_node->tag)];
+	if(likely(node == NULL)) {
+		node = new_node;
+	} else {
+		while(node->next != NULL) {
+			node = node->next;
 		}
+		node->next = new_node;
 	}
+	return 0;
+	
+}
 
-	/* Add new tag node to the right */
-	if(compare > 0) {
-		if(root->right == NULL) {
-			root->right = new_node;
-			return 0;
-		} else {
-			return add_node(root->right, tag);	
-		}
+void remove(struct tag_node **head, const char *tag, unsigned long inode_num) {
+	struct tag_node *node;
+	node = find_node(head, tag);
+	if(node) {
+		remove_entry(node->e, inode_num);
+		/* TODO: Clear this pointer if there are no more table entries.
+		*  This could be done by modifying remove_entry to return count. */
 	}
 }
 
-int insert(struct tag_tree_node *root, const char *tag, const struct inode_entry *i)
+int insert(struct tag_node **head, const char *tag, const struct inode_entry *i)
 {
-	struct tag_tree_node *node;
-	node = find_node(root, tag);
+	struct tag_node *node;
+	int e;
+	node = find_node(head, tag);
 	if(!node) {
 		/* Create empty table element */
-		node = kmalloc(sizeof(struct tag_tree_node), GFP_KERNEL);
-		node->left = NULL;
-		node->right = NULL;
+		node = kmalloc(sizeof(struct tag_node), GFP_KERNEL);
+		if(!node)
+			return -ENOMEM;
+		node->next = NULL;
 		node->e = new_element();
 		strlcpy(node->tag, tag, MAX_TAG_LEN);
-		if(add_node(root, node) < 0) {
+		/* TODO: Add tag id */
+		if((e = add_node(head, node)) < 0) {
 			delete_element(node->e);
 			kfree(node->tag);
 			kfree(node);
+			return e;
 		}
 	}
 
-	/* TODO: Add tag to inode hash */
-
 	insert_entry(node->e, i);
+	return 0;
 }
 
-struct table_element * get_inodes(struct tag_tree_node *root, char* tag) 
+struct table_element * get_inodes(struct tag_node **head, char* tag) 
 {
-	struct tag_tree_node n = find_node(root, tag);
+	struct tag_node *n = find_node(head, tag);
+	if(!n)
+		return NULL;
 	return n->e;
 }
+
+struct tag_node * create_table(void)
+{
+	struct tag_node *head;
+	head = kmalloc(sizeof(head) * (1<<NUM_HASH_BITS), GFP_KERNEL);
+	return head;
+}
+
